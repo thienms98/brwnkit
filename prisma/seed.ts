a2e7dcee859f582ce7336f6129d940638317e12d
Brown <thienms98@gmail.com>
2026-06-11 16:33:17 +0700

    add: prisma seed, login page

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,24 @@
 import "dotenv/config";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
+import bcrypt from "bcryptjs";
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
+  const salt = await bcrypt.genSalt(11);
+  const hash = await bcrypt.hash("123123", salt);
+
   const admin = await prisma.account.upsert({
     where: { username: "admin" },
     update: {},
     create: {
       username: "admin",
-      email: "alice",
-      password: "123123",
+      email: "admin",
+      password: hash,
       role: "ADMIN"
     }
   });
@@ -22,7 +26,8 @@ async function main() {
   const product = await prisma.product.create({
     data: {
       title: "Gaming Chair X1",
-      price: 532.5
+      price: 532.5,
+      slug: "gaming-chair-x1"
     }
   });
 
@@ -30,11 +35,13 @@ async function main() {
     data: [
       {
         title: "seat",
-        productId: product.id
+        productId: product.id,
+        partName: "seat"
       },
       {
         title: "legs",
-        productId: product.id
+        productId: product.id,
+        partName: "legs"
       }
     ]
   });
